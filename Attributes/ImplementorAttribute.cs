3e49dc50eda9cb4ebbc679e3192409a530dc37cd
npolyak <nick.polyak@verizon.net>
2018-06-17 12:33:15 -0400

    added a number of attributes

---
+++ Attributes/ImplementorAttribute.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NP.Concepts.Attributes
+{
+    [AttributeUsage(AttributeTargets.Property, AllowMultiple=false, Inherited=true)]
+    public class ImplementorAttribute : Attribute
+    {
+        public Type ImplementorType { get; }
+
+        public ImplementorAttribute(Type implementorType)
+        {
+
+        }
+    }
+}
---
+++ Attributes/SuppressWrappingAttribute.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NP.Concepts.Attributes
+{
+    [AttributeUsage(AttributeTargets.Property, AllowMultiple = true, Inherited = true)]
+    public class SuppressWrappingAttribute : Attribute
+    {
+        public string MemberName { get; }
+
+        public SuppressWrappingAttribute(string memberName)
+        {
+            MemberName = memberName;
+        }
+    }
+}
