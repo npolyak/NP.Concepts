79f9a394578844b241163b02ba428373f1b74bff
npolyak <nick.polyak@verizon.net>
2018-09-03 21:43:02 -0400

    increased the version

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("NP.Concepts")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Non visual helpers and behaviors")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Nick Polyak")]
 [assembly: AssemblyProduct("NP.Concepts")]
 [assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.1.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
