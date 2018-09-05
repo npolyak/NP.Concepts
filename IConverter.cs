2689b9acf531264be6242e266c89f9a243527be6
npolyak <nick.polyak@verizon.net>
2018-09-05 19:00:42 -0400

    added a file that was missing in git

---
+++ IConverter.cs
@@ -0,0 +1,9 @@
+﻿namespace NP.Concepts
+{
+    public interface IConverter<TIn, TOut>
+    {
+        TOut Convert(TIn val);
+    }
+
+    public interface IConverter : IConverter<object, object> { }
+ }
