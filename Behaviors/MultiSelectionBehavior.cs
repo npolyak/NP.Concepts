569264206eae9575e9a4087c926fae1494edd7eb
npolyak <nick.polyak@verizon.net>
2019-01-27 22:45:58 -0500

    added a couple of behaviors.

---
+++ Behaviors/MultiSelectionBehavior.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+namespace NP.Concepts.Behaviors
+{
+    public class MultiSelectionBehavior<T>
+        where T : ISelectableItem<T>
+    {
+        private IEnumerable<T> _collection;
+
+        private ObservableCollection<T> _selectedItems = new ObservableCollection<T>();
+
+        IDisposable _behaviorDisposable = null;
+
+        public IEnumerable<T> Collection
+        {
+            get => _collection;
+
+            set
+            {
+                if (ReferenceEquals(_collection, value))
+                    return;
+
+                _collection = value;
+
+                _behaviorDisposable =
+                    _collection?.AddBehavior
+                    (
+                        (item) => item.IsSelectedChanged += Item_IsSelectedChanged,
+                        (item) => item.IsSelectedChanged -= Item_IsSelectedChanged
+                    );
+            }
+        }
+
+        private void Item_IsSelectedChanged(T selectedOrUnselectedItem)
+        {
+            if (selectedOrUnselectedItem.IsSelected)
+            {
+                _selectedItems.Add(selectedOrUnselectedItem);
+            }
+            else
+            {
+                _selectedItems.Remove(selectedOrUnselectedItem);
+            }
+        }
+
+        public IEnumerable<T> SelectedItems { get => _selectedItems; } 
+    }
+}
---
+++ Behaviors/SelectableItemWithData.cs
@@ -0,0 +1,42 @@
+﻿using NP.Utilities;
+
+namespace NP.Concepts.Behaviors
+{
+    public class SelectableItemWithData<T> : SelectableItem<SelectableItemWithData<T>>
+    {
+        public T Data { get; }
+
+        public SelectableItemWithData(T data)
+        {
+            Data = data;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj is SelectableItemWithData<T> selectableItemWithData)
+            {
+                return Data.ObjEquals(selectableItemWithData.Data);
+            }
+
+            return false;
+        }
+
+        public override int GetHashCode()
+        {
+            return Data.GetHashCodeExtension();
+        }
+
+        public override string ToString()
+        {
+            return Data?.ToString();
+        }
+    }
+
+    public static class SelectableItemWithDataUtils
+    {
+        public static SelectableItemWithData<T> ToSelectableItem<T>(this T data)
+        {
+            return new SelectableItemWithData<T>(data);
+        }
+    }
+}
