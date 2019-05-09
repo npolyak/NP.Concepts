701112b017b6c20e9fbaffb29ad7309f793d29cd
npolyak <nick.polyak@verizon.net>
2019-05-09 17:59:51 -0400

    added Cache concept

--- Behaviors/MultiSelectionWithSearchVm.cs
+++ Behaviors/MultiSelectionWithSearchVm.cs
@@ -1,83 +1,20 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 
 namespace NP.Concepts.Behaviors
 {
-    public abstract class MultiSelectionWithSearchVm<T> : 
-        VMBase, IMultiSelectionWithSearchVm
+    public class ShallowMultiSelectionWithSearchVm<T> :
+        MultiSelectionWithSearchBase<T>
         where T : ISelectableItem<T>, ISearchable
     {
-        private string _lowerCaseSearchStr;
 
-        IDisposable _disposableBehavior;
-
-        public IEnumerable AllItemsToDisplay => AllItems;
-
-        private IEnumerable<T> _allItems;
-        public IEnumerable<T> AllItems
-        {
-            get => _allItems;
-
-            protected set
-            {
-                if (ReferenceEquals(_allItems, value))
-                    return;
-
-                _disposableBehavior?.Dispose();
-
-                _allItems = value;
-
-                if (_allItems != null)
-                {
-                    _disposableBehavior = _allItems.AddBehavior
-                    (
-                        item => item.IsSelectedChanged += Item_IsSelectedChanged,
-                        item => item.IsSelectedChanged -= Item_IsSelectedChanged
-                    );
-                }
-            }
-        }
-
-        private void Item_IsSelectedChanged(T obj)
+        protected override void OnItemSelectionChanged(T item)
         {
             OnPropertyChanged(nameof(SelectedItems));
             OnPropertyChanged(nameof(SelectedItemsToDisplay));
         }
 
-        #region SearchStr Property
-        private string _searchStr;
-        public string SearchStr
-        {
-            get
-            {
-                return this._searchStr;
-            }
-            set
-            {
-                if (this._searchStr == value)
-                {
-                    return;
-                }
-
-                this._searchStr = value;
-                this.OnPropertyChanged(nameof(SearchStr));
-
-                _lowerCaseSearchStr = _searchStr?.ToLower();
-
-                foreach(T item in AllItems)
-                {
-                    item.SearchStr = _lowerCaseSearchStr;
-                }
-            }
-        }
-        #endregion SearchStr Property
-
-        public IEnumerable<T> SelectedItems =>
+        public override IEnumerable<T> SelectedItems =>
             AllItems.Where(item => item.IsSelected).ToList();
-
-        public IEnumerable SelectedItemsToDisplay =>
-            SelectedItems;
     }
 }
