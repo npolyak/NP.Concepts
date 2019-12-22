a4a7907569cd013f394416a92c6fee041aba34a2
npolyak <nick.polyak@verizon.net>
2019-12-22 15:15:04 -0500

    added a file that should have been added before

---
+++ XamlResourceInfo.cs
@@ -0,0 +1,20 @@
+﻿using NP.Utilities;
+
+namespace NP.Concepts
+{
+    public class XamlResourceInfo
+    {
+        public string ResourceDictionaryUrl { get; }
+        public object ResourceKey { get; }
+
+        public XamlResourceInfo(string resourceDictionaryUrl, object resourceKey)
+        {
+            ResourceDictionaryUrl = resourceDictionaryUrl;
+            ResourceKey = resourceKey;
+        }
+
+        public bool IsValid =>
+            (!ResourceDictionaryUrl.IsNullOrEmpty()) && 
+            (!ResourceKey.IsNullOrEmptyCollection());
+    }
+}
