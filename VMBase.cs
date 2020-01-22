3346bd42eb14e530b7af9c5c9512113bca4ca39b
npolyak <nick.polyak@verizon.net>
2020-01-22 16:52:51 -0500

    added IVMBaseContainer<TExtra> interface.

--- VMBase.cs
+++ VMBase.cs
@@ -1,16 +1,15 @@
 ﻿using NP.Concepts.Attributes;
+using NP.Utilities.BasicInterfaces;
 using System.ComponentModel;
 
 namespace NP.Concepts
 {
-    public interface IVMBase : INotifyPropertyChanged
+    public interface IVMBase : INotifyPropertyChanged, IPropChangedContainer
     {
-        void OnPropertyChanged(string propertyName);
     }
 
     public class VMBase : IVMBase
     {
-
         #region INotifyPropertyChanged Members
         [EventThisIdx]
         public event PropertyChangedEventHandler PropertyChanged;
@@ -21,9 +20,32 @@ namespace NP.Concepts
             (this as IVMBase).OnPropertyChanged(propertyName);
         }
 
-        void IVMBase.OnPropertyChanged(string propertyName)
+        void IPropChangedContainer.OnPropertyChanged(string propertyName)
         {
             this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+
+    public interface IVMBaseContainer<TExtra> : IVMBase
+        where TExtra : INotifyPropertyChanged, new()
+    {
+        TExtra Extra { get; set; }
+
+        private void Connect(INotifyPropertyChanged notifiable)
+        {
+            notifiable.PropertyChanged += Notifiable_PropertyChanged;
+        }
+
+        void Notifiable_PropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            OnPropertyChanged(e.PropertyName);
+        }
+
+        void Init()
+        {
+            Extra = new TExtra();
+
+            Connect(Extra);
         }
     }
 }
